33c1071e0dcc48cd3cc1b92d6738ca4cdf42402c
Mark Peters <mapeters101@earthlink.net>
2024-11-18 15:51:06 -0500

    Lesson197:  Complete work on lesson from 4:10.

--- lesson197.py
+++ lesson197.py
@@ -1,14 +1,30 @@
 
-answer = input("Do you want to continue? Enter yes of no:").lower()
-if answer == 'yes':
-    print("We'll move on.")
-elif answer == 'no':
-    print("We'll stop.")
+# balance = 100
+# price = 50
+#
+# if balance >= price:
+#     answer = input("Do you want to continue? Enter yes of no:").lower()
+#     if answer == 'yes':
+#         print("We'll move on.")
+#     elif answer == 'no':
+#         print("We'll stop.")
+#     else:
+#         print('Invalid answer.')
+#     new_balance = balance - price
+#     print(f'You can book the flight and your new balance will be {new_balance}.')
+# else:
+#     print(f'Insufficient finds!  Please deposit {price - balance}')
+#
+# print('Other instructions ...')
+# x = 10
+# # ...
+
+x = 10
+if x <= 10:
+    print('x less than or equal to 10.')
+elif x == 10:
+    print('x is equal to 10.')
 else:
-    print('Invalid answer.')
-
-
-
-
+    print('x is greater than 10.')
 
 
