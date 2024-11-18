01cb619bc4b3d8665c4c93f6f30835ce6caca2e5
Mark Peters <mapeters101@earthlink.net>
2024-11-18 15:42:14 -0500

    Lesson196:  Work on lesson.

---
+++ lesson196.py
@@ -0,0 +1,18 @@
+
+balance = 100
+price = 50
+
+if balance >= price:
+    new_balance = balance - price
+    print(f'You can book the flight and your new balance will be {new_balance}.')
+else:
+    print(f'Insufficient finds!  Please deposit {price - balance}')
+
+print('Other instructions ...')
+x = 10
+# ...
+
+
+
+
+
