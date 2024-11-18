fa3a4f9848e65542c0cd3e01d672f5d0f12172b9
Mark Peters <mapeters101@earthlink.net>
2024-11-18 16:14:31 -0500

    Lesson198:  Work on lesson to 4:15.

---
+++ lesson198.py
@@ -0,0 +1,31 @@
+print(issubclass(bool, int))
+print(int(True))
+print(int(True)+int(True))
+print(int(False))
+print(hex(id(True)))
+print(hex(id(4>3)))
+print((4>3) == True)
+print((4>3) is True)
+print()
+
+print(True == 1)
+print(True is 1)
+print()
+print(hex(id(True)))
+print(hex(id(1)))
+print()
+
+print(3 == False)
+print('4' == 4)
+print()
+
+a, b = 1, 2
+print(a)
+print(b)
+print(a > b, b < a)
+print(a > b, b == 2)
+
+
+
+
+
