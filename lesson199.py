e812405e6a36c9031faead8d48338307bd2a8ac3
Mark Peters <mapeters101@earthlink.net>
2024-11-18 18:22:01 -0500

    Lesson199:  Work on lesson.

---
+++ lesson199.py
@@ -0,0 +1,29 @@
+age = 6
+print(age > 6)
+print(age > 0 and age < 18)
+print(0 < age < 18)
+print()
+
+age = 20
+print(age > 6)
+print(age > 0 and age < 18)
+print(0 < age < 18)
+print()
+
+
+age = 20
+name = 'Daniel'
+print(age > 0 and age < 28 and name == 'Daniel')
+print(age > 0 and age < 28 and 'Danny' in name)
+print()
+
+age, name = 6, 'Daniel'
+print(age < 3 or name == 'Andrei')
+print(age < 3 or name == 'Daniel')
+print(age == 6 or name == 'Andrei')
+print(age != 6 or 'dan' in name)
+print(age != 6 or 'dan' in name.lower())
+
+
+
+
