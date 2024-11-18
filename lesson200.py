0bf0c50ef610628fd5411751ba132def1cb54642
Mark Peters <mapeters101@earthlink.net>
2024-11-18 18:30:22 -0500

    Lesson200:  Work on lesson.

---
+++ lesson200.py
@@ -0,0 +1,16 @@
+
+print(not 2 == 2)
+age = 6
+print(not age > 20)
+name = 'Daniel'
+print(age < 18 and not name == 'Andrei')
+print(not age == 6 or name == 'Daniel')
+print(not (age == 6 or name == 'Daniel'))
+print(age < 6 and name == 'Daniel')
+print(age < 6 and name == 'Daniel' or len(name) == 6)
+print('-'*80)
+
+
+a, b = 10, 20
+print(a > 100 and b == 20)
+print(a == 10 or b < 0)
