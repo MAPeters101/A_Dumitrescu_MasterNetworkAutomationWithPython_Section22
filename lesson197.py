8eeb04b1cc4071f59a68022ebae85279660e69d2
Mark Peters <mapeters101@earthlink.net>
2024-11-18 15:46:24 -0500

    Lesson197:  Work on lesson to 4:10.

---
+++ lesson197.py
@@ -0,0 +1,14 @@
+
+answer = input("Do you want to continue? Enter yes of no:").lower()
+if answer == 'yes':
+    print("We'll move on.")
+elif answer == 'no':
+    print("We'll stop.")
+else:
+    print('Invalid answer.')
+
+
+
+
+
+
